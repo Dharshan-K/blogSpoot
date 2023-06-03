d4eb1356f41924a1a6fb174f208680cc9212869e
Dharshan K <kdharshanprog@gmail.com>
2023-06-03 23:18:00 +0530

    changes done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./db.js");
+const path = require("path");
 require("dotenv").config();
 
 app.use(cors());
@@ -14,5 +15,14 @@ connectDB();
 
 app.use("/blog", require("./routers/blogRoutes"));
 app.use("/user", require("./routers/userRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
 
 app.listen(5000, () => console.log("listening to port 5000"));
