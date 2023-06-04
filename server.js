862db904ad58067320eb8aa5ee232d01087c6901
Dharshan K <kdharshanprog@gmail.com>
2023-06-04 12:11:41 +0530

    server.js modified

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ require("dotenv").config();
 console.log("before cors");
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://blogspot-frontend-dual.onrender.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
