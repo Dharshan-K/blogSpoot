afb0eef01a2830e3fe6b478739899a5d60c1169e
Dharshan K <kdharshanprog@gmail.com>
2023-06-04 09:23:46 +0530

    cors error rectified

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ require("dotenv").config();
 console.log("before cors");
 app.use(cors());
 console.log("after cors");
-app.use(function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
-  );
-});
-console.log("after headers");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
