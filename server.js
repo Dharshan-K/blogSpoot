f04d8b8755dd00f090addc1ba844ae2a8a83964f
Dharshan K <kdharshanprog@gmail.com>
2023-06-04 14:27:21 +0530

    server.js modified

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     "https://blogspot-frontend-dual.onrender.com"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
